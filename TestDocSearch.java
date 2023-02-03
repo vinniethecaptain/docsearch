c25c2ee55515c7bae0297058149a96d4ad4e6ea3
vinniethecaptain <v1bui@ucsd.edu>
2023-02-02 17:45:40 -0800

    fixed testSearch

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -16,7 +16,8 @@ public class TestDocSearch {
     public void testSearch() throws URISyntaxException, IOException {
         Handler h = new Handler("./written_2/");
         URI rootPath = new URI("http://localhost/search?q=sushi");
-        String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
+        //String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
+        String expect = "Found 3 paths:\n.\\written_2\\travel_guides\\berlitz1\\WhatToJapan.txt\n.\\written_2\\travel_guides\\berlitz1\\WhereToJapan.txt\n.\\written_2\\travel_guides\\berlitz2\\California-WhereToGo.txt";
         assertEquals(expect, h.handleRequest(rootPath));
     }
 }
